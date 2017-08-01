793e7fdab3db7c54f252d61d257af3e9af204fe4
Marian FX <marian.focsa@outlook.com>
2017-08-01 17:08:09 +0300

    Add project files.

---
+++ FX_Photo_Discovery_Angular/Controllers/BaseAPIController.cs
@@ -0,0 +1,10 @@
+﻿using FX_Photo_Discovery_Angular.DbContext;
+using System.Web.Http;
+
+namespace FX_Photo_Discovery_Angular.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+        protected readonly UserDBEntities UserDb = new UserDBEntities();
+    }
+}
---
+++ FX_Photo_Discovery_Angular/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FX_Photo_Discovery_Angular.MvcApplication" Language="C#" %>
