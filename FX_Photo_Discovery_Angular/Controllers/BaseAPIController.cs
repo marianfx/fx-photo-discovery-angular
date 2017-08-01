9c9278bb5b085e2267dff570f2a9dc26e6295fb1
Marian FX <marian.focsa@outlook.com>
2017-08-01 17:42:06 +0300

    Updated BaseAPiController and added UserApi.

--- FX_Photo_Discovery_Angular/Controllers/BaseAPIController.cs
+++ FX_Photo_Discovery_Angular/Controllers/BaseAPIController.cs
@@ -1,10 +1,21 @@
-﻿using FX_Photo_Discovery_Angular.DbContext;
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using FX_Photo_Discovery_Angular.DbContext;
 using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace FX_Photo_Discovery_Angular.Controllers
 {
-    public class BaseAPIController : ApiController
+    public class BaseApiController : ApiController
     {
         protected readonly UserDBEntities UserDb = new UserDBEntities();
+
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
     }
 }
---
+++ FX_Photo_Discovery_Angular/Controllers/UserApiController.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using FX_Photo_Discovery_Angular.DbContext;
+
+namespace FX_Photo_Discovery_Angular.Controllers
+{
+    public class UserApiController : BaseApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(UserDb.APPUSERS.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody] APPUSER value)
+        {
+            UserDb.APPUSERS.Add(value);
+            return ToJson(UserDb.SaveChanges());
+        }
+
+        public HttpResponseMessage Put([FromBody] APPUSER value)
+        {
+            UserDb.Entry(value).State = EntityState.Modified;
+            return ToJson(UserDb.SaveChanges());
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            var result = UserDb.APPUSERS.FirstOrDefault(x => x.ID == id);
+            UserDb.APPUSERS.Remove(result);
+            return ToJson(UserDb.SaveChanges());
+        }
+    }
+}
